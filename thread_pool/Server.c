ef284339c64f5b1d31f0ee015d964faa9a522f2b
dragon-web <1582943265@qq.com>
2020-07-29 11:21:16 +0800

    thread_pool/

---
+++ thread_pool/Server.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+
+#define SERV_PORT 8888
+
+
+int main(void)
+{ 
+ int lfd,cfd;
+ lfd = Socket(AF_INET,SOCK_STREAM,0);
+ 
+
+
+
+  return 0;
+}
